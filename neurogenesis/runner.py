635cfb4f88ce783a52613f11ab41cdd3bbe3daf7
Julius Flohr <julius.flohr@uni-due.de>
2016-07-28 06:12:47 -0700

    fixed last bugs in simulation setup

--- neurogenesis/runner.py
+++ neurogenesis/runner.py
@@ -2,7 +2,8 @@
 from subprocess import call
 
 def run_simulation(path_to_execute_binary, path_to_hosts_file, nr_ranks, simulations):
-    command = "mpirun -np " + nr_ranks + "--hostfile " + path_to_hosts_file + " " + path_to_execute_binary + " "
+    command = "/usr/bin/mpirun -np " + str(nr_ranks) + " --hostfile " + path_to_hosts_file + " " + path_to_execute_binary + " "
     for hash in simulations.keys():
         command += hash + " "
-    call(command)
+    print(command) 
+    call(command, shell=True)
