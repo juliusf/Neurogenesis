cce74e2e35e2475f1060f3df0df1a402e44edd79
Julius Flohr <julius.flohr@uni-due.de>
2016-08-04 13:13:08 +0200

    first version of plotter

---
+++ neurogenesis/plotter.py
@@ -0,0 +1,27 @@
+
+import pickle
+import matplotlib.pyplot as plt
+
+class SimulationConfig():
+    def __init__(self):
+        self.x_limits = (0,100,10)
+        self.y_limits = (0, 100, 10)
+
+        self.x_axis_parameter = ""
+        self.y_axis_parameter = ""
+
+def load_plot_config(path):
+
+
+    for line in config_file:
+        if line.lstrip()[0] is "#": #comment
+            continue
+
+        tokens = line.strip().split(" ")
+
+def plot_simulations(simulations, plot_script_path):
+    file = open(plot_script_path, "rb")
+    plot_script = pickle.load(file)
+    file.close()
+    exec plot_script
+
