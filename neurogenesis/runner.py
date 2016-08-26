3ee6a948532c31d53881d3690dd44b98bdc79514
Julius Flohr <julius.flohr@uni-due.de>
2016-08-26 16:20:40 +0200

    modified runner to create simulation list file

--- neurogenesis/runner.py
+++ neurogenesis/runner.py
@@ -1,8 +1,20 @@
 
 from subprocess import call
+import os
 
 def run_simulation(path_to_execute_binary, path_to_hosts_file, nr_ranks, simulations):
-    command = "/usr/bin/mpirun -np " + str(nr_ranks) + " --hostfile " + path_to_hosts_file + " " + path_to_execute_binary + " "
-    for hash in simulations.keys():
-        command += hash + " "
+    file = "/tmp/distsim_sims.list"
+    write_list_of_sims(file, simulations)
+    command = "/usr/bin/mpirun -np " + str(nr_ranks) + " --hostfile " + path_to_hosts_file + " " + path_to_execute_binary + " " + file
     call(command, shell=True)
+
+
+def write_list_of_sims(file, simulations):
+    try:
+        os.remove(file)
+    except OSError:
+        pass
+    f = open(file, "w+")
+    for hash in simulations.keys():
+        f.write(hash + "\n")
+    f.close()
