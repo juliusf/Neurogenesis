23ddea2bd1bc587cb0a26632986af8f02f02f123
Julius Flohr <julius.flohr@uni-due.de>
2017-05-24 09:56:48 +0200

    deleted old code

--- neurogenesis/demux.py
+++ neurogenesis/demux.py
@@ -155,13 +155,3 @@ def create_dynamic_line(line):
         dline.tail_part = tail_tokens[1]
     return dline
 
-def parse_tokens(tokens):
-    more_tokens = []
-    for token in tokens:
-        if "=" in token: # assignment
-            assignment_tokens = token.split("=")
-            more_tokens.append(Token(0, TokenTypes.SYMBOL), )
-
-def dynamic_line_range(line):
-    #has '..', optional step parameter
-    pass
